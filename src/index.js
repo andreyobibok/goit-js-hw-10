839a1e5421d4627ed27a6d0df63b4c4371725818
Andrey Obibok <andreyobibok@gmail.com>
2023-01-06 14:33:56 +0200

    git fix 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
